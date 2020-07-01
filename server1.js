734d773ebd215fc1c4b437e3d88646de012d398f
Yadira Tamarit <ystamaritq@gmail.com>
2020-06-30 20:37:36 -0500

    added ex 3

--- server1.js
+++ server1.js
@@ -3,51 +3,61 @@
 var express = require("express");
 
 var app = express();
-// var PORT = 3000;
 var PORT = process.env.PORT || 3000;
+
 // Data
 // ===========================================================
-var yoda = {
-	name: "Yoda",
-	role: "Jedi Master",
-	age: 900,
-	forcePoints: 2000,
-};
-
-var darthmaul = {
-	name: "Darth Maul",
-	role: "Sith Lord",
-	age: 200,
-	forcePoints: 1200,
-};
-
-// Create one more data entry for the character Obi Wan Kenobi.
-// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
-
-var obiwankenobi = {
-	name: "Obi Wan Kenobi",
-	role: "Lord",
-	age: 340,
-	forcePoints: 3000,
-};
+var characters = [
+	{
+		routeName: "yoda",
+		name: "Yoda",
+		role: "Jedi Master",
+		age: 900,
+		forcePoints: 2000,
+	},
+	{
+		routeName: "darthmaul",
+		name: "Darth Maul",
+		role: "Sith Lord",
+		age: 200,
+		forcePoints: 1200,
+	},
+	{
+		routeName: "obiwankenobi",
+		name: "Obi Wan Kenobi",
+		role: "Jedi Master",
+		age: 55,
+		forcePoints: 1350,
+	},
+];
 
 // Routes
 // ===========================================================
+
 app.get("/", function (req, res) {
 	res.send("Welcome to the Star Wars Page!");
 });
 
-app.get("/yoda", function (req, res) {
-	res.json(yoda);
+// What does this route do?
+app.get("/api/characters", function (req, res) {
+	return res.json(characters);
 });
 
-app.get("/darthmaul", function (req, res) {
-	res.json(darthmaul);
-});
+// What does this route do?
+app.get("/api/characters/:character", function (req, res) {
+	// What does this code do?
+	var chosen = req.params.character;
+	console.log(chosen);
 
-// Create a new Express route that leads users to the new Obi Wan Kenobi Data
-app.get("/obiwankenobi", function (req, res) {
-	res.json(obiwankenobi);
+	// What does this code do?
+	for (var i = 0; i < characters.length; i++) {
+		if (chosen === characters[i].routeName) {
+			return res.json(characters[i]);
+		}
+	}
+
+	// What does this code do?
+	return res.send("No character found");
 });
 
 // Listener
